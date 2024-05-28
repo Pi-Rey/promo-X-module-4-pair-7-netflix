1c1e40c4d05e6db9b08219a463ad7360a346118a
Pi Rey <pilar.rrdgz@gmail.com>
2024-05-28 11:14:55 +0200

    add data

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Por favor marca a continuación los ejercicios que hayáis terminado:
 **4.1 Fundamentos de MySQL:**
 
 - [x] 1.  Crea la base de datos.
-- [ ] 2.  Inserta valores a la bases de datos.
+- [x] 2.  Inserta valores a la bases de datos.
 - [x] 3.  Crea un repositorio en Github.
 
 **4.2 CRUD MySQL:**
--- bd/netflix_script.sql
+++ bd/netflix_script.sql
@@ -25,3 +25,20 @@ lastname varchar(45) not null,
 country varchar(45) not null,
 birthday date
 );
+INSERT INTO movies (title, genre, image, category, `year`)
+VALUES ("Pulp Fiction", "Crimen", "https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg","Top 10","1994"), 
+("La vita è bella", "Comedia", "https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg", "Top 10", "1996");
+
+INSERT INTO movies (title, genre, image, category, `year`)
+VALUES ("Forrest Gump", "Comedia", "https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg", "Top 10", "1994");
+
+INSERT INTO users (`user`, `password`, `name`, email, plan_details)
+VALUES ("laura_dev", "laura", "Laura", "laura@gmail.com", "Standard"),
+("maria_dev", "maria", "Maria", "maria@gmail.com", "Standard"),
+("ester_dev", "ester", "Estar", "ester@gmail.com", "Standard");
+
+INSERT INTO actors (`name`, lastname, country, birthday)
+VALUES ("Tom", "Hanks", "Estados Unidos", "1956-07-09"), 
+("Roberto", "Benigni", "Italia", "1952-10-27"),
+("John", "Travolta", "Estados Unidos", "1954-02-18");
+
